f292dd934fd8e5c16e820889d0fa3eb99607b2b2
Ignacio García <nachoacad@gmail.com>
2023-03-15 16:31:49 -0300

    Eccomerce con carrito

---
+++ js/products.js
@@ -0,0 +1,38 @@
+const productos = [
+    {
+        id: 1,
+        nombre: 'AMD Ryzen 5 1600',
+        precio: 42100,
+        img: 'https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_17898_Procesador_AMD_Ryzen_5_1600_AF_Zen__12nm_AM4_Wraith_Stealth_Cooler_187bb9ab-grn.jpg',
+    },
+    {
+        id: 2,
+        nombre: 'Intel Core i3 12100',
+        precio: 50400,
+        img: 'https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_31717_Procesador_Intel_Core_i3_12100_4.3GHz_Socket_1700_Alder_Lake_28683e1f-grn.jpg',
+    },
+    {
+        id: 3,
+        nombre: 'HyperX Cloud Revolver 7.1',
+        precio: 35380,
+        img: 'https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_30992_Auriculares_HyperX_Cloud_Revolver_7.1_3dd11be0-grn.jpg',
+    },
+    {
+        id: 4,
+        nombre: 'Gabinete Kolink Inspire K3',
+        precio: 20050,
+        img: 'https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_34567_Gabinete_Kolink_Inspire_K3_RGB__M-ATX_Vidrio_Templado_7306ef99-grn.jpg',
+    },
+    {
+        id: 5,
+        nombre: 'Fuente ASUS TUF 550W',
+        precio: 28600,
+        img: 'https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_23310_Fuente_ASUS_TUF_550W_80_Plus_Bronze_550B_cfcb396e-grn.jpg',
+    },
+    {
+        id: 6,
+        nombre: 'Memoria Team DDR4 8GB 2666MHz',
+        precio: 16199,
+        img: 'https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_13313_Memoria_Team_DDR4_8GB_2666MHz_T-Force_Vulcan_Z_Gray__07ea04cc-grn.jpg',
+    },
+]
